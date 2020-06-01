7a3f418c4876e98e77468941bec2a9581e7e9369
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2020-06-01 11:58:58 +0200

    Doing some Angular Tests

--- src/app/title.component.ts
+++ src/app/title.component.ts
@@ -10,21 +10,33 @@ export class TitleComponent {
 
     peoples = [{
         name: "Fabio",
-        age: 30
+        age: 30,
+        value: true
     },
-    {
+    { 
         name: "Robin",
-        age: 25
+        age: 25,
+        value: false
     },
     {
         name: "Adalberto",
-        age: 40
+        age: 40,
+        value: true
+
 
     },{
         name: "ADSDAS",
-        age: null
+        age: null,
+        value: false
 
     }]
 
+    addClass(peoples){
+        const addClass = document.querySelector(".col");
+        addClass.classList.add(peoples);
+        // let index = peoples.findIndex(x => x.LastName === name);
+        // console.log(index);
+        // return index;
 
+    }
 }
