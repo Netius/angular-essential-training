b988c54c46b55ceef91034dcb67fae7013357910
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2020-06-11 14:17:18 +0200

    Last used

--- src/app/title.component.ts
+++ src/app/title.component.ts
@@ -7,36 +7,38 @@ import { Component } from '@angular/core';
 })
 
 export class TitleComponent {
+    
 
     peoples = [{
+        id: 0,
         name: "Fabio",
         age: 30,
         value: true
     },
-    { 
+    {   id: 1,
         name: "Robin",
         age: 25,
         value: false
     },
-    {
+    {   id: 2,
         name: "Adalberto",
         age: 40,
         value: true
 
 
-    },{
+    },{id: 3,
         name: "ADSDAS",
         age: null,
         value: false
 
     }]
 
-    addClass(peoples){
-        const addClass = document.querySelector(".col");
-        addClass.classList.add(peoples);
-        // let index = peoples.findIndex(x => x.LastName === name);
-        // console.log(index);
-        // return index;
+    addClass(name){
+        const addClass = document.querySelectorAll(".col h3");
+
+        console.log(name);
+        
+        
 
     }
 }
